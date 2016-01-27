d650ef4f7f32d0c210cc8af270b5ab5bddd36e4e
markDeveloping <mark.developing@gmail.com>
2016-01-27 12:16:14 +0000

    Introduce jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,41 @@
 //= require turbolinks
 //= require_tree .
 
+
 $(document).ready(function(){
-    $(".block-label").click(function(){
-        $(this).toggleClass("focused");
-    });
+	function variables() {
+   blindSift = $('input[name="blind-sift"]:checked').val();
+   cvCurrent = $('input[name="CV-upload"]:checked').val();
+   supportCurrent = $('input[name="supporting-upload"]:checked').val();
+   cvNew = $('input[name="CV-new"]:checked').val();
+   skillsNew = $('input[name="skills-new"]:checked').val();
+	};
+
+	function addRemove() {
+		if(blindSift === "Yes" || blindSift === "Not selected"){
+			$('.current').addClass('hidden');
+			$('.new').removeClass('hidden');
+		}
+		else if(blindSift === "No") {
+			$('.new').addClass('hidden');
+			$('.current').removeClass('hidden');
+		};
+
+		if(supportCurrent === "No" || supportCurrent === "Not selected"){
+			$('.enter-details').addClass('hidden');
+		}
+		else if(supportCurrent === "Yes"){
+			$('.enter-details').removeClass('hidden');
+		}
+		else {
+			$('.enter-details').removeClass('hidden');
+		};
+	};
+
+	$('input').click(function(){
+		variables();
+		addRemove();
+	});
+		variables();
+		addRemove();
 });
